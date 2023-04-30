c58151b347ea22c6db64e4583e41da7df58a1140
Hayormikun <akomolafefavour@gmail.com>
2023-04-30 21:24:18 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "Rubik": "Rubik"
       },
+
+      colors: {
+        'VDarkGray': '#2b2b2b',
+        'DarkGray': '#969696'
+      }
     },
   },
   plugins: [],
